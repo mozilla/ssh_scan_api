15fbbc744c77f3b71e4c5d04293229eab7acec43
Jonathan Claudius <jclaudius@mozilla.com>
2017-05-12 16:48:41 -0400

    Throw logging to stderr instead of stdout

--- examples/client.py
+++ examples/client.py
@@ -1,25 +1,28 @@
 from __future__ import print_function
-
+import sys
 import requests
 import json
 import time
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 api_server = "https://sshscan.rubidus.com"
 
-print("[+] Submitting scan request for ssh.mozilla.com")
+eprint("[+] Submitting scan request for ssh.mozilla.com")
 resp = requests.post(api_server + "/api/v1/scan?target=ssh.mozilla.com")
-print("[+] Got %s %s %s" % (resp.status_code, resp.reason, resp.text))
+eprint("[+] Got %s %s %s" % (resp.status_code, resp.reason, resp.text))
 
 resp_json = json.loads(resp.text)
 res_url = "%s/api/v1/scan/results?uuid=%s" % (api_server, resp_json["uuid"])
 
 while True:
-    print("[+] Checking for scan results")
+    eprint("[+] Checking for scan results")
     scan_results = requests.get(res_url)
     scan_results_json = json.loads(scan_results.text)
     if 'ssh_scan_version' in scan_results_json:
-        print(scan_results_json)
+        print(scan_results.text)
         exit()
     else:
-        print("[+] Backing off for a sec to let scan to complete")
+        eprint("[+] Backing off for a sec to let scan to complete")
         time.sleep(1)
