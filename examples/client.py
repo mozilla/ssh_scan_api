5ec048753ed3dd06dd9471a52af5f44194e4379e
Jonathan Claudius <jclaudius@mozilla.com>
2017-05-12 16:34:11 -0400

    Make pep8 compliant

--- examples/client.py
+++ examples/client.py
@@ -7,19 +7,19 @@ import time
 api_server = "https://sshscan.rubidus.com"
 
 print("[+] Submitting scan request for ssh.mozilla.com")
-submit_response = requests.post(api_server + "/api/v1/scan?target=ssh.mozilla.com")
-print("[+] Got %s %s %s" % (submit_response.status_code, submit_response.reason, submit_response.text))
+resp = requests.post(api_server + "/api/v1/scan?target=ssh.mozilla.com")
+print("[+] Got %s %s %s" % (resp.status_code, resp.reason, resp.text))
 
-submit_response_json = json.loads(submit_response.text)
-results_url = "%s/api/v1/scan/results?uuid=%s" % (api_server,submit_response_json["uuid"])
+resp_json = json.loads(resp.text)
+res_url = "%s/api/v1/scan/results?uuid=%s" % (api_server, resp_json["uuid"])
 
 while True:
-  print("[+] Checking for scan results")
-  scan_results = requests.get(results_url)
-  scan_results_json = json.loads(scan_results.text)
-  if 'ssh_scan_version' in scan_results_json:
-    print(scan_results_json)
-    exit()
-  else:
-    print("[+] Backing off for a sec to let scan to complete")
-    time.sleep(1)
+    print("[+] Checking for scan results")
+    scan_results = requests.get(res_url)
+    scan_results_json = json.loads(scan_results.text)
+    if 'ssh_scan_version' in scan_results_json:
+        print(scan_results_json)
+        exit()
+    else:
+        print("[+] Backing off for a sec to let scan to complete")
+        time.sleep(1)
