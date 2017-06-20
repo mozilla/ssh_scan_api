8800d1ff5636b70c25f4be270b27e2c6c18e1400
Jonathan Claudius <jclaudius@mozilla.com>
2017-06-20 08:46:28 -0400

    Re-add RACK_ENV context for testing

--- lib/ssh_scan_api/api.rb
+++ lib/ssh_scan_api/api.rb
@@ -10,14 +10,16 @@ require 'ssh_scan_api/database'
 
 module SSHScan
   class API < Sinatra::Base
-    configure do
-      set :authentication, false
-      config_file = File.join(Dir.pwd, "./config/api/config.yml")
-      opts = YAML.load_file(config_file)
-      opts["config_file"] = config_file
-      set :db, SSHScan::Database.from_hash(opts)
-      set :environment, :production
-      set :allowed_ports, [22]
+    if ENV['RACK_ENV'] == 'test'
+      configure do
+        set :authentication, false
+        config_file = File.join(Dir.pwd, "./config/api/config.yml")
+        opts = YAML.load_file(config_file)
+        opts["config_file"] = config_file
+        set :db, SSHScan::Database.from_hash(opts)
+        set :environment, :production
+        set :allowed_ports, [22]
+      end
     end
 
     # Configure all the secure headers we want to use
