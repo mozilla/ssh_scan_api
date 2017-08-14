9c5ba40a42d19a4dc87557d9c9378c6d9ef25d00
Jonathan Claudius <claudijd@yahoo.com>
2017-08-14 11:31:12 -0400

    Add initial draft of monitoring function

--- monitoring/handler.py
+++ monitoring/handler.py
@@ -8,6 +8,24 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
-    #TODO
-    # 1.) Make a get request(s) to the SSH API endpoint and see if it's still running
-    # 2.) If it's still running, do nothing, if it is, then pop an alert (like a pushover notification or something)
+
+    import requests
+
+    message = ""
+
+    try
+	  response = requests.get("https://sshscan.rubidus.com/")
+
+	  if response.status_code != 200
+	    message = "Got non-200 response from SSH Scan API base route"
+	  elif response.text != "See API documentation here: https://github.com/mozilla/ssh_scan_api/wiki/ssh_scan-Web-API"
+	    message = "Got unexpected content from SSH Scan API base route"
+
+	except requests.exceptions.RequestException
+	  message = "Unable to connect to SSH Scan API"
+
+	#TODO: Send message somewhere (maybe pushover), if it's set (to start, we'll just report to log for dev/testing purposes)
+	if message
+	  logger.info("Failing State: " + message + " " + str(current_time))
+	else
+   	  logger.info("Passing State: " + message + " " + str(current_time))
