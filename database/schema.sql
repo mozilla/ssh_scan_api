ea3716919b6be11cbc359a5d57a5aea92355f634
Jonathan Claudius <jclaudius@mozilla.com>
2018-01-10 17:34:53 -0500

    Remove setup stuff from schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,15 +1,3 @@
--- Defines postgres bootstrap
-
---Create the 'sshobs' postgres user account
-CREATE ROLE sshobs WITH LOGIN PASSWORD 'auserpassword'; -- need to parameterize the password, so it's not baked into the schema bootstrap
-ALTER ROLE sshobs CREATEDB; 
-
--- Create the ssh_observatory database
-CREATE DATABASE ssh_observatory;
-
--- Give the sshobser user full access to the ssh_observatory database
-GRANT ALL PRIVILEGES ON DATABASE ssh_observatory TO sshobs;
-
 -- Define the scan table
 CREATE TABLE IF NOT EXISTS scans (
   id SERIAL PRIMARY KEY,
