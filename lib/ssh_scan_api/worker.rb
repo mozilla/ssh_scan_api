3a7803475ba5de97b47028c214f88ced8f9b7fc5
Jonathan Claudius <jclaudius@mozilla.com>
2018-01-23 11:52:45 -0500

    Spec out worker behavior

--- lib/ssh_scan_api/worker.rb
+++ lib/ssh_scan_api/worker.rb
@@ -2,6 +2,7 @@ require 'ssh_scan/scan_engine'
 require 'ssh_scan_api/version'
 require 'ssh_scan_api/models/scan'
 require 'logger'
+require 'yaml'
 
 class SSHScan::Worker
   def initialize()
@@ -12,9 +13,21 @@ class SSHScan::Worker
     @poll_interval = 1
   end
 
-  def setup_db_connection
-    ActiveRecord::Base.establish_connection({adapter: "sqlite3", database: "foo.sqlite3"})
+  def set_environment(environment)
     ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+    config = YAML.load_file(File.join(File.dirname(__FILE__),"../../config/database/database.yml"))
+
+    case environment
+    when "test"
+      ActiveRecord::Base.establish_connection(config["test"])
+    when "development"
+      ActiveRecord::Base.establish_connection(config["development"])
+    when "production"
+      ActiveRecord::Base.establish_connection(config["production"])
+    else
+      raise "Unsupported environment specified, acceptable environments are test, development, or production"
+    end
   end
 
   def do_work
@@ -49,8 +62,6 @@ class SSHScan::Worker
   end
 
   def run!
-    setup_db_connection
-
     loop do
       #begin
         do_work
---
+++ spec/ssh_scan_api/worker_spec.rb
@@ -0,0 +1,89 @@
+require 'spec_helper'
+require 'ssh_scan_api/worker'
+require 'ssh_scan'
+require 'json'
+
+describe SSHScan::Worker do
+
+  before do :each  
+    @worker = SSHScan::Worker.new
+    @worker.set_environment("test")
+
+    # Start fresh with a new database every time
+    SSHScan::Scan.destroy_all
+  end
+
+  it "should have sane default behavior" do
+    expect(@worker.instance_variable_get(:@worker_id)).to match(/^[\w]+{8}-[\w]+{4}-[\w]+{4}-[\w]+{4}-[\w]+{12}$/)
+    expect(@worker.instance_variable_get(:@fingerprint_database_path)).to be_kind_of(::String)
+    expect(@worker.instance_variable_get(:@policy_path)).to be_kind_of(::String)
+    expect(@worker.instance_variable_get(:@poll_interval)).to eql(1)
+  end
+
+  it "should pull items off the queue and complete them" do
+    # Manually add a scan to the queue
+    scan = SSHScan::Scan.new do |s|
+      s.scan_id = SecureRandom.uuid
+      s.creation_time = Time.now
+      s.target = "sshscan.rubidus.com"
+      s.port = 22
+      s.state = "QUEUED"
+      s.save
+    end
+
+    # Verify the scan is in the queue
+    scan_from_queue = SSHScan::Scan.find_by("scan_id": scan.scan_id)
+    expect(scan_from_queue).to be_kind_of(SSHScan::Scan)
+
+    @worker.do_work
+    completed_scan = SSHScan::Scan.find_by("scan_id": scan.scan_id)
+    expect(completed_scan.state).to eql("COMPLETED")
+    expect(completed_scan.grade).to eql("A")
+    expect(completed_scan.worker_id).to match(/^[\w]+{8}-[\w]+{4}-[\w]+{4}-[\w]+{4}-[\w]+{12}$/)
+    expect(completed_scan.raw_scan).to be_kind_of(::String)
+    expect(JSON.parse(completed_scan.raw_scan)).to be_kind_of(::Hash)
+  end
+
+  it "should pull items off the queue and complete them in order" do
+    scan1 = SSHScan::Scan.new do |s|
+      s.scan_id = SecureRandom.uuid
+      s.creation_time = Time.now
+      s.target = "sshscan.rubidus.com"
+      s.port = 22
+      s.state = "QUEUED"
+      s.save
+    end
+
+    scan2 = SSHScan::Scan.new do |s|
+      s.scan_id = SecureRandom.uuid
+      s.creation_time = Time.now
+      s.target = "sshscan.rubidus.com"
+      s.port = 22
+      s.state = "QUEUED"
+      s.save
+    end
+
+    # Verify the scans are in the queue
+    scan1_from_queue = SSHScan::Scan.find_by("scan_id": scan1.scan_id)
+    expect(scan1_from_queue).to be_kind_of(SSHScan::Scan)
+    scan2_from_queue = SSHScan::Scan.find_by("scan_id": scan2.scan_id)
+    expect(scan1_from_queue).to be_kind_of(SSHScan::Scan)
+
+    @worker.do_work
+    completed_scan = SSHScan::Scan.find_by("scan_id": scan1.scan_id)
+    expect(completed_scan.state).to eql("COMPLETED")
+    expect(completed_scan.grade).to eql("A")
+    expect(completed_scan.worker_id).to match(/^[\w]+{8}-[\w]+{4}-[\w]+{4}-[\w]+{4}-[\w]+{12}$/)
+    expect(completed_scan.raw_scan).to be_kind_of(::String)
+    expect(JSON.parse(completed_scan.raw_scan)).to be_kind_of(::Hash)
+
+    @worker.do_work
+    completed_scan = SSHScan::Scan.find_by("scan_id": scan2.scan_id)
+    expect(completed_scan.state).to eql("COMPLETED")
+    expect(completed_scan.grade).to eql("A")
+    expect(completed_scan.worker_id).to match(/^[\w]+{8}-[\w]+{4}-[\w]+{4}-[\w]+{4}-[\w]+{12}$/)
+    expect(completed_scan.raw_scan).to be_kind_of(::String)
+    expect(JSON.parse(completed_scan.raw_scan)).to be_kind_of(::Hash)
+  end
+
+end
