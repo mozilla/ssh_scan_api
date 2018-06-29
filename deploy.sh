f606d177da36c26964a6cb9f8d15354c2b70b24f
Jonathan Claudius <jclaudius@mozilla.com>
2018-06-29 10:41:15 -0400

    Remove old deploy script, it's no longer relevant

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-docker-compose build --no-cache && \
-docker stop $(docker ps -a -q) && \
-# For debug only...
-# docker-compose up
-docker-compose up -d --scale base=0 --scale database=1 --scale api=1 --scale worker=3 --no-recreate
